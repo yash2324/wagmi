fe146f19b72b4cc80589625b08b7657fecc8b61e
Yash Gupta <76815371+yash2324@users.noreply.github.com>
2023-10-30 21:39:50 +0530

    Add files via upload

---
+++ HelloWorld/src/com/yash/oops2.java
@@ -0,0 +1,17 @@
+package com.yash;
+
+public class oops2 {
+    public static void main(String[] args) {
+        int[] numbers= new int[5];
+        String[] names= new String[5];
+        int[] rno= new int[5];
+        String[] name=new String[5];
+        float[] marks= new float[5];
+        Student[] students= new Student[5];
+    }
+    class Student{
+        int rno ;
+        String name;
+        float[] marks;
+    }
+}
